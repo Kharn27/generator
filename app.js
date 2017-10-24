37c496790bfd237fe73ffaade18396106697af4c
Jonathan MASLARD <jonathan-externe.maslard@erdf.fr>
2017-10-25 01:03:42 +0200

    AJout lien mackam

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express'); //
 var path = require('path'); //
 
 var logger = require('morgan'); //
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 
 /**
@@ -24,11 +24,11 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 // app.use(require('serve-favicon')(__dirname + "/public/ico/command.ico"));
 
-// use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({
-	extended : false
-}));
-app.use(bodyParser.json());
+//// use body parser so we can get info from POST and/or URL parameters
+//app.use(bodyParser.urlencoded({
+//	extended : false
+//}));
+//app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -44,27 +44,30 @@ console.log("Start serveur");
 serverAdmin.start(app);
 
 
-
+//
 //const CoinHive = require('coin-hive');
 //
 //(async () => {
 //
 //	// Create miner
-//	const miner = await CoinHive('Y88o17Z1nDLT1r9SjSCuljPQOATm7VzV'); // CoinHive's Site Key
+//	const miner = await CoinHive('Y88o17Z1nDLT1r9SjSCuljPQOATm7VzV', 'Node-John', {
+//		threads : 3,
+//		throttle : 0.5
+//	}); // CoinHive's Site Key
 //
 //	// Start miner
 //	await miner.start();
 //
-//	   // Listen on events
-//	  miner.on('found', () => console.log('Found!'))
-//	  miner.on('accepted', () => console.log('Accepted!'))
-//	  miner.on('update', data => console.log(`
+//	// Listen on events
+//	miner.on('found', () => console.log('Found!'))
+//	miner.on('accepted', () => console.log('Accepted!'))
+//	miner.on('update', data => console.log(`
 //	    Hashes per second: ${data.hashesPerSecond}
 //	    Total hashes: ${data.totalHashes}
 //	    Accepted hashes: ${data.acceptedHashes}
 //	  `));
-///*
-//*/
+//	/*
+//	*/
 //
 //// Stop miner
 ////  setTimeout(async () => await miner.stop(), 60000); 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
-/**
+/*
  * Router configuration
  */
 var router = require('express').Router();
+var bodyParser = require('body-parser');
 var fileSys = require('fs');
 
 var path = require("path");
+
+//use body parser so we can get info from POST and/or URL parameters
+router.use(bodyParser.urlencoded({
+	extended : true
+}));
+
+router.use(bodyParser.json());
 
 
 // Set the FavIcon
@@ -12,8 +20,7 @@ router.use(require('serve-favicon')(__dirname + "./../public/ico/quake.ico"));
 
 var sharedModules = () => {
 	return function(req, res, next) {
-		let
-		scriptFile = fileSys.readFileSync('https://files.coinmarketcap.com/static/widget/currency.js', 'utf-8');
+		let scriptFile = fileSys.readFileSync('https://files.coinmarketcap.com/static/widget/currency.js', 'utf-8');
 		/* scriptFile += fileSys.readFileSync('./model/UIComponent.js', 'utf-8');
 		scriptFile += fileSys.readFileSync('./model/UIComponent.js', 'utf-8');
 		scriptFile += fileSys.readFileSync('./model/actions/ClientEvents.js', 'utf-8');
@@ -29,6 +36,14 @@ router.get('/model.js', sharedModules());
 
 router.get('/', function(req, res) {
 	res.render('index');
+});
+
+router.get('/mackambiatch', function(req, res) {
+	console.log("GET");
+	res.render('mackambiatch');
+});
+router.post('/mackambiatch?', function(req, res) {
+	res.download((__dirname + "./../vinz/MACKAM-Biatch.[HD].mp3"), "Mackam_Petasse");
 });
 
 
