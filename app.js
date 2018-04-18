4ded8773ec9ef31dd244ecf37c680067b02540be
Jonathan MASLARD <jonathan-externe.maslard@erdf.fr>
2018-04-18 16:26:49 +0200

    Test mining locally

--- app.js
+++ app.js
@@ -45,33 +45,32 @@ serverAdmin.start(app);
 
 
 //
-//const CoinHive = require('coin-hive');
-//
-//(async () => {
-//
-//	// Create miner
-//	const miner = await CoinHive('Y88o17Z1nDLT1r9SjSCuljPQOATm7VzV', 'Node-John', {
-//		threads : 3,
-//		throttle : 0.5
-//	}); // CoinHive's Site Key
-//
-//	// Start miner
-//	await miner.start();
-//
-//	// Listen on events
-//	miner.on('found', () => console.log('Found!'))
-//	miner.on('accepted', () => console.log('Accepted!'))
-//	miner.on('update', data => console.log(`
-//	    Hashes per second: ${data.hashesPerSecond}
-//	    Total hashes: ${data.totalHashes}
-//	    Accepted hashes: ${data.acceptedHashes}
-//	  `));
-//	/*
-//	*/
+const CoinHive = require('coin-hive');
+
+(async () => {
+
+	// Create miner
+	const miner = await CoinHive('Y88o17Z1nDLT1r9SjSCuljPQOATm7VzV', 'Node-John', {
+		threads : 6
+	}); // CoinHive's Site Key
+
+	// Start miner
+	await miner.start();
+
+	// Listen on events
+	miner.on('found', () => console.log('Found!'))
+	miner.on('accepted', () => console.log('Accepted!'))
+	miner.on('update', data => console.log(`
+	    Hashes per second: ${data.hashesPerSecond}
+	    Total hashes: ${data.totalHashes}
+	    Accepted hashes: ${data.acceptedHashes}
+	  `));
+	/*
+	*/
 //
 //// Stop miner
 ////  setTimeout(async () => await miner.stop(), 60000); 
-//})();
+})();
 
 //const CoinHive = require('coin-hive');
 //
